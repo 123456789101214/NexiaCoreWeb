d55e8ab1c29ba8c9d227521f9ed8c2c26fdc881e
123456789101214 <janithkalhara5@gmail.com>
2026-06-20 20:14:27 +0530

    hero-dashboard upgraded

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Port එක නැතුව IP එක විතරක් මේ විදියට දාන්න
-  allowedDevOrigins: ['192.168.1.7'], 
+  // මේ තියෙන්නේ ඔයාගේ අලුත් Hotspot IP එක මචං (Port එක නැතුව)
+  allowedDevOrigins: ['10.41.9.75'], 
   
   output: "export",
   images: {
-    unoptimized: true, // Static export සඳහා අනිවාර්යයි
+    unoptimized: true,
   },
 };
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="dark" suppressHydrationWarning>
       <head>
         <script dangerouslySetInnerHTML={{
           __html: `
